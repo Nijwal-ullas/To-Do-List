db24636a8e7b02db35e11a568d16d738fcece579
nijwal <nijwal12@gmail.com>
2026-06-21 15:58:03 +0530

    initialing project

---
+++ todo-app/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <input type="text" placeholder="Enter a task" />
+      <button>Add Task</button>
+    </div>
+  );
+}
+
+export default App;
